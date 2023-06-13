978366dd8df4601d1dd9066ce4335c57cbb12e00
Carlos Iego <carlosiego2017@gmail.com>
2023-06-13 00:21:17 -0300

    40% products update

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -53,41 +53,58 @@ class ProductsController {
         }
 
         let codeCurrent = Number(req.params.code)
-        let { video } = req.body
+        let { video, code: newCode } = req.body
         let { originalname: name, size } = req.file
-        let newCode = Number(name.split('.')[0])
-        // 2° Verificando se a imagem a ser modificada existe!
-        let imageBD = await ProductsRepository.findByCode(codeCurrent)
-        let imageServer = fs.statSync(`./public/upload/imagesProducts/${imageBD.name}`)
-        return res.json({ imageBD: !imageBD, imageServer: !imageServer })
 
-    }
+        // 2° Verificando se existe novo código!
+        if (!newCode) {
+            return res.status(404).json({ error: 'Código é requerido' })
+        }
+
+        // 3° Verificando se a imagem a ser modificada existe!
+        let imageBD = await ProductsRepository.findByCode(codeCurrent)
+
+        if (!imageBD) {
+            return res.status(404).json({ error: 'Imagem não encontrada' })
+        }
+
+        // 4° Verificando se o novo código já está em uso!
+        let newCodeInUse = false
+        if (codeCurrent !== newCode) {
+            newCodeInUse = await ProductsRepository.findByCode(newCode)
+        }
+        if (newCodeInUse) return res.status(400).json({ error: 'O código já está em uso' })
+
+        let imageUpdated = await ProductsRepository.update({{ name, codeCurrent, newCode, video, size }})
+
+        res.json({ imageUpdated })
+}
 
     async deleteImage(req, res) {
-        let code = req.params.code
-        let isImage = await ImageProducts.findOne({ where: { code: code } })
+    let code = req.params.code
+    let isImage = await ImageProducts.findOne({ where: { code: code } })
 
-        if (isImage) {
-            await ImageProducts.destroy({ where: { code } })
-                .then(() => {
-                    fs.unlinkSync(`./public/upload/imagesProducts/${isImage.name}`)
-                    return res.json({
-                        error: false,
-                        message: 'Imagem excluída com sucesso!'
-                    })
-                }).catch(() => {
-                    return res.status(400).json({
-                        error: true,
-                        message: 'Erro, não foi possivel deletar!'
-                    })
+    if (isImage) {
+        await ImageProducts.destroy({ where: { code } })
+            .then(() => {
+                fs.unlinkSync(`./public/upload/imagesProducts/${isImage.name}`)
+                return res.json({
+                    error: false,
+                    message: 'Imagem excluída com sucesso!'
                 })
-        } else {
-            return res.status(400).json({
-                error: true,
-                message: 'Erro, não existe imagem com o código: ' + code
+            }).catch(() => {
+                return res.status(400).json({
+                    error: true,
+                    message: 'Erro, não foi possivel deletar!'
+                })
             })
-        }
+    } else {
+        return res.status(400).json({
+            error: true,
+            message: 'Erro, não existe imagem com o código: ' + code
+        })
     }
+}
 }
 
 module.exports = new ProductsController()
