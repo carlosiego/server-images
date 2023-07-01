3e947f7a3c0eac9aabafcfe82fd727ac0b699c06
Carlos <carlosiego2017@gmail.com>
2023-07-01 08:35:38 -0300

    remove secret api

--- src/routes/LocationsRoutes.js
+++ src/routes/LocationsRoutes.js
@@ -6,9 +6,9 @@ const uploadLocations = require('../middlewares/uploadLocations')
 
 LocationsRoutes
     // ========================================= IMAGES LOCATIONS ====================================================================
-    .post(`/${process.env.SECRET_API}/images/locations`, uploadLocations.single('image'), LocationsController.createImage)
-    .get(`/${process.env.SECRET_API}/images/locations/code/:code`, LocationsController.listImages)
-    .put(`/${process.env.SECRET_API}/images/locations/name/:name`, uploadLocations.single('image'), LocationsController.updateImage)
-    .delete(`/${process.env.SECRET_API}/images/locations/name/:name`, LocationsController.deleteImage)
+    .post(`/images/locations`, uploadLocations.single('image'), LocationsController.createImage)
+    .get(`/images/locations/code/:code`, LocationsController.listImages)
+    .put(`/images/locations/name/:name`, uploadLocations.single('image'), LocationsController.updateImage)
+    .delete(`/images/locations/name/:name`, LocationsController.deleteImage)
 
 module.exports = LocationsRoutes
--- src/routes/ProductsRoutes.js
+++ src/routes/ProductsRoutes.js
@@ -4,10 +4,10 @@ const ProductsRoutes = express.Router()
 const uploadProducts = require('../middlewares/uploadProducts')
 
 ProductsRoutes
-    .post(`/${process.env.SECRET_API}/images/products`, uploadProducts.single('image'), ProductsController.createImage)
-    .get(`/${process.env.SECRET_API}/images/products/code/:code`, ProductsController.listImage)
-    .put(`/${process.env.SECRET_API}/images/products/code/:code`, uploadProducts.single('image'), ProductsController.updateImage)
-    .delete(`/${process.env.SECRET_API}/images/products/code/:code`, ProductsController.deleteImage)
+    .post(`/images/products`, uploadProducts.single('image'), ProductsController.createImage)
+    .get(`/images/products/code/:code`, ProductsController.listImage)
+    .put(`/images/products/code/:code`, uploadProducts.single('image'), ProductsController.updateImage)
+    .delete(`/images/products/code/:code`, ProductsController.deleteImage)
 
 
 module.exports = ProductsRoutes
