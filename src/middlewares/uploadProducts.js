8b9e81bb6f1783b12403cbac6733f681f7c688fa
Carlos Iego <carlosiego2017@gmail.com>
2023-07-24 22:59:08 -0300

    add path uploads linux

--- src/middlewares/uploadProducts.js
+++ src/middlewares/uploadProducts.js
@@ -3,20 +3,21 @@ const path = require('path')
 const { v4 } = require('uuid')
 
 const isLinux = process.platform === 'linux'
-const pathLinux = path.resolve(__dirname, '..', '..', 'uploads', process.env.DIR_IMAGES_PRODUCTS) // SISTEMA DE PRODUÇÃO
+
+const pathLinux = path.resolve('/', 'home', 'uploads', process.env.DIR_IMAGES_PRODUCTS) // SISTEMA DE PRODUÇÃO
 const pathWindows = path.resolve('C:', 'uploads', process.env.DIR_IMAGES_PRODUCTS) // SISTEMA DE DESENVOLVIMENTO
 
 const uploadProducts = multer({
-  storage: multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, (isLinux ? pathLinux : pathWindows));
+	storage: multer.diskStorage({
+		destination: (req, file, cb) => {
+			cb(null, (isLinux ? pathLinux : pathWindows));
 
-    },
+		},
 
-    filename: (req, file, cb) => {
-      cb(null, v4() + '-' + file.originalname);
-    },
-  })
+		filename: (req, file, cb) => {
+			cb(null, v4() + '-' + file.originalname);
+		},
+	})
 
 });
 
